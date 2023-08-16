4984a246476c5b030cfc88b25ca8a6a721381147
carrottechplus <blohzzang@gmail.com>
2023-08-16 11:43:02 +0900

    이미지 사이즈 용량 최적화

--- pages/index.js
+++ pages/index.js
@@ -30,7 +30,9 @@ export default function Home({ meals }) {
 									alt={item.strMeal}
 									priority
 									fill
-									quality={50}
+									quality={100}
+									sizes='(max-width:768px) 100vw, (max-width:1200px) 50vw, 33vw'
+									// 이미지 속성이 fill로 지정되어 있을 경우, frame크기에 상관없이 초기 로딩시 전체 브라우저 크기의 100vw크기의 용량으로 가져오기 때문에 브라우저 폭에 출력될 크기를 지정해서 이미지 성능 향상
 									style={{ objectFit: 'cover' }}
 								/>
 							</div>
@@ -49,7 +51,7 @@ export default function Home({ meals }) {
 
 export async function getStaticProps() {
 	// props로 데이터를 넘길때에는, data안쪽의 값까지 뽑아낸 다음에 전달!!
-	const { data } = await axios.get('/filter.php?c=Seafood');
+	const { data } = await axios.get('/filter.php?a=American');
 	console.log('data fetching on Server', data);
 	return {
 		props: data,
