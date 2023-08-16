0d0372c358429213052b8f37059dd69fce9b1f98
carrottechplus <blohzzang@gmail.com>
2023-08-16 10:54:37 +0900

    pre-rendering시 서버쪽에 data property안쪽의 데이터를 props로 전달

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,13 @@
 import Head from 'next/head';
 import styles from './Home.module.scss';
 import clsx from 'clsx';
+import axios from 'axios';
+axios.defaults.baseURL = 'https://www.themealdb.com/api/json/v1/1';
 
-export default function Home() {
+//www.themealdb.com/api/json/v1/1/filter.php?c=Seafood
+
+export default function Home(props) {
+	console.log(props);
 	return (
 		<>
 			<Head>
@@ -16,4 +21,14 @@ export default function Home() {
 			</main>
 		</>
 	);
+}
+
+export async function getStaticProps() {
+	// props로 데이터를 넘길때에는, data안쪽의 값까지 뽑아낸 다음에 전달!!
+	const { data } = await axios.get('/filter.php?c=Seafood');
+	console.log('data fetching on Server', response.data);
+	return {
+		props: data,
+		revalidate: 10, //이 부분 없으면 ssg
+	};
 }
