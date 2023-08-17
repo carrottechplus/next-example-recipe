c5ca79513bbe51427806ea4cbe3235bf3e82bc71
carrottechplus <blohzzang@gmail.com>
2023-08-17 12:38:46 +0900

    type 분기처리해서 class 스타일링

--- pages/index.js
+++ pages/index.js
@@ -14,15 +14,17 @@ export default function Home({ meals }) {
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
 			<main className={clsx(styles.main)}>
-				<Title url={'/abc'} className={clsx(styles.txt, styles.logo)} style={{ color: 'violet', hoverColor: 'green' }}>
+				<Title url={'/abc'} className={clsx(styles.txt)} type={'logo'}>
+					{/* style={{ color: 'gray', hoverColor: 'green' }}> */}
 					{/* 
           컬러값 inline으로 넣으려면,,
           - style에 color 적용시 hover 까지 덮어써짐
           - 아예 hover 값까지 같이 그룹으로 전달
           - style 객체로 color 값 자체를 전달하지 않게되면 module.scss에 있는 style 적용됨.
            */}
-					Hello
+					Whereas disregard and contempt for human rights have resulted
 				</Title>
+				<Title type={'slogan'}>slogan</Title>
 			</main>
 		</>
 	);
