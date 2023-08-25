f183d4cc961ba6dd5b3e5fc90aa0cc3b2c581e2a
carrottechplus <blohzzang@gmail.com>
2023-08-25 11:06:40 +0900

    swiper 컴포넌트에 title데이터 출력 및 스타일링

--- components/organisms/Swiper/Swiper.jsx
+++ components/organisms/Swiper/Swiper.jsx
@@ -3,6 +3,7 @@ import clsx from 'clsx';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore, { Autoplay } from 'swiper';
 import 'swiper/css';
+import Title from '@/components/atoms/text/Title';
 
 //npm i swiper@9
 function SwiperWrap({ recipe, category }) {
@@ -10,9 +11,19 @@ function SwiperWrap({ recipe, category }) {
 	console.log('category!!', category);
 
 	return (
-		<figrue className={clsx(styles.visual)}>
-			<Swiper className={clsx(styles.swiper)}></Swiper>
-		</figrue>
+		<figure className={clsx(styles.visual)}>
+			<Swiper className={clsx(styles.swiper)}>
+				{recipe.map((item) => (
+					<SwiperSlide key={item.idMeal} className={clsx(styles.swiperSlide)}>
+						<div>
+							<Title tag={'h3'} url={'/'} type={'slogan'}>
+								{item.strMeal}
+							</Title>
+						</div>
+					</SwiperSlide>
+				))}
+			</Swiper>
+		</figure>
 	);
 }
 
