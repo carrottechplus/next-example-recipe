446c06103ad2c6ce1d6356d01a5003b15cd7ca23
carrottechplus <blohzzang@gmail.com>
2023-08-28 11:59:07 +0900

    쿼리 스트링으로 값 전달 방법 설명

--- components/organisms/Swiper/Swiper.jsx
+++ components/organisms/Swiper/Swiper.jsx
@@ -51,6 +51,7 @@ function SwiperWrap({ recipe, category }) {
 										url={`/find-recipe/${item.idMeal}?name=${item.strMeal}`}
 										type={'slogan'}
 									>
+										{/* 다이나믹 라우팅으로 기본 id값 전달외에 ?뒤에 쿼리 스트링값을 전달하면 해당 값을 다이나믹 라우팅이 적용되는 페이지 안에서 비구조화할당으로 받음 */}
 										{item.strMeal.length > 25 ? item.strMeal.substr(0, 25) : item.strMeal}
 									</Title>
 								</div>
--- pages/find-recipe/[id].jsx
+++ pages/find-recipe/[id].jsx
@@ -2,10 +2,11 @@ import { useRouter } from 'next/router';
 
 function Detail() {
 	const router = useRouter();
-	const { id, name } = router.query;
+	const { id, name, url } = router.query;
 	return (
 		<section>
 			<h2>{name}</h2>
+			<p>{url}</p>
 		</section>
 	);
 }
